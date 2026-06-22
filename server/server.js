04887e213948e67212c4512456171da6bb7ad7cf
Neha <nehapawar174@gmail.com>
2026-06-22 22:14:01 +0530

    Update CORS configuration

--- server/server.js
+++ server/server.js
@@ -16,9 +16,7 @@ const allowedOrigins = ['http://localhost:5173']
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: ["http://localhost:5173",
-    "https://FRONTEND.vercel.app"], 
-    credentials: true}));
+app.use(cors({origin:"http://localhost:5173", credentials: true}));
 
 //API endpoints
 app.get('/', (req, res)=> res.send("API Working"));
